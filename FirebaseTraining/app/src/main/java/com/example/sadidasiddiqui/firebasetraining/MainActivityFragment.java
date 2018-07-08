6ff6f7f0d78acfebf5f812daaa255cef47e4ac65
SADIDA SIDDIQUI <sadidasiddiqui@SADIDAs-MBP.home>
2018-07-08 15:31:30 -0400

    Firebase project with screenshots of changes

---
+++ FirebaseTraining/app/src/main/java/com/example/sadidasiddiqui/firebasetraining/MainActivityFragment.java
@@ -0,0 +1,52 @@
+package com.example.sadidasiddiqui.firebasetraining;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.*;
+
+import static android.content.ContentValues.TAG;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View fragmentView = inflater.inflate(R.layout.fragment_main, container, false);
+        final TextView firebase_View = (TextView) fragmentView.findViewById(R.id.firebaseview);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!");
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                firebase_View.setText(value);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+        return fragmentView;
+    }
+
+}
